1723723fb7a0fbc640a378513431a400de8adb30
Student <jsitbon@qcc.cuny.edu>
2019-11-06 11:39:51 -0500

    Categories

--- My-Webapp/week7/midterm4/js/spa.js
+++ My-Webapp/week7/midterm4/js/spa.js
@@ -6,31 +6,54 @@ app.config(function($routeProvider) {
       controller:'HomeController',
       templateUrl: 'app/views/home.html'
     })
+
   .when('/works',
     {
       controller: 'WorksController',
       templateUrl: 'app/views/works.html'
     })
+
     .when('/news',
       {
         controller: 'NewsController',
         templateUrl: 'app/views/news.html'
       })
+
    .when('/contact',
       {
         controller: 'ContactController',
         templateUrl: 'app/views/contact.html'
       })
+
    .when('/about',
       {
         controller: 'StoryController',
         templateUrl: 'app/views/about.html'
       })
+
   .when('/services',
      {
        controller: 'AdamController',
        templateUrl: 'app/views/services.html'
      })
+
+   .when('/services',
+        {
+          controller: 'AdamController',
+          templateUrl: 'app/views/services.html'
+        })
+
+    .when('/services',
+           {
+             controller: 'AdamController',
+             templateUrl: 'app/views/services.html'
+           })
+
+     .when('/services',
+              {
+                controller: 'AdamController',
+                templateUrl: 'app/views/services.html'
+              })
 
      .when('/test',
         {
@@ -38,21 +61,39 @@ app.config(function($routeProvider) {
           templateUrl: 'app/views/test.html'
         })
 });
+
+
+
 app.controller('WorksController', function($scope){
   $scope.customerID = 5;
 });
+
+
+
 app.controller('NewsController', function($scope){
   $scope.customerID = 5;
 });
+
+
+
 app.controller('StoryController', function($scope){
   $scope.customerID = 5;
 });
+
+
+
 app.controller('ContactController', function($scope){
   $scope.customerID = 5;
 });
+
+
+
 app.controller('AdamController', function($scope){
   $scope.customerID = 5;
 });
+
+
+
 app.controller('HomeController', function($scope) {
   $scope.customers = [
       {"id": 1, "name":"Ted", "total": 5.996},

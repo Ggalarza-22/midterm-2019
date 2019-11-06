2f9736bee64a9689efb2081171bbcbeab04e2b21
Student <jsitbon@qcc.cuny.edu>
2019-11-06 12:01:03 -0500

    spa js

--- My-Webapp/week7/midterm4/js/spa.js
+++ My-Webapp/week7/midterm4/js/spa.js
@@ -12,6 +12,50 @@ app.config(function($routeProvider) {
       controller: 'WorksController',
       templateUrl: 'app/views/works.html'
     })
+    app.controller('WorksController', ['$scope', function($scope) {
+      $scope.title = 'This Month\'s Bestsellers';
+      $scope.promo = 'The most popular books this month.';
+      $scope.products = [
+      	{
+        	name: 'The Book of Trees',
+        	price: 19,
+        	pubdate: new Date('2014', '03', '08'),
+        	cover: 'img/the-book-of-trees.jpg',
+        	likes: 0,
+        	dislikes: 0
+      	},
+      	{
+        	name: 'Program or be Programmed',
+        	price: 8,
+        	pubdate: new Date('2013', '08', '01'),
+        	cover: 'img/program-or-be-programmed.jpg',
+        	likes: 0,
+        	dislikes: 0
+      	},
+      	{
+        	name: 'Harry Potter & The Prisoner of Azkaban',
+        	price: 11.99,
+        	pubdate: new Date('1999', '07', '08'),
+        	cover: 'http://upload.wikimedia.org/wikipedia/en/b/b4/Harry_Potter_and_the_Prisoner_of_Azkaban_(US_cover).jpg',
+        	likes: 0,
+        	dislikes: 0
+      	},
+      	{
+        	name: 'Ready Player One',
+        	price: 7.99,
+        	pubdate: new Date('2011', '08', '16'),
+        	cover: 'http://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg',
+        	likes: 0,
+        	dislikes: 0
+      	}
+      ];
+      $scope.plusOne = function(index) {
+      	$scope.products[index].likes += 1;
+    	};
+    	$scope.minusOne = function(index) {
+      	$scope.products[index].dislikes += 1;
+    	};
+    }]);
 
     .when('/news',
       {
@@ -37,29 +81,13 @@ app.config(function($routeProvider) {
        templateUrl: 'app/views/services.html'
      })
 
-   .when('/services',
-        {
-          controller: 'AdamController',
-          templateUrl: 'app/views/services.html'
-        })
+   .when('/test',
+      {
+        controller: 'AdamController',
+        templateUrl: 'app/views/test.html'
+      })
 
-    .when('/services',
-           {
-             controller: 'AdamController',
-             templateUrl: 'app/views/services.html'
-           })
 
-     .when('/services',
-              {
-                controller: 'AdamController',
-                templateUrl: 'app/views/services.html'
-              })
-
-     .when('/test',
-        {
-          controller: 'AdamController',
-          templateUrl: 'app/views/test.html'
-        })
 });
 
 
